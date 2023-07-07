c00bdadc9ff6d52308de1162b1d267094aa45a2a
Niraj Shaw <nirazshaw156@gmail.com>
2023-07-07 12:01:20 +0530

    error solved

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -44,16 +44,16 @@ const DetailsBanner = ({ video, crew }) => {
           {!!data && (
             <React.Fragment>
               <div className="backdrop-img">
-                <Img src={url.backdrop + data.backdrop_path} />
+                <Img src={url.backdrop + data?.backdrop_path} />
               </div>
               <div className="opacity-layer"></div>
               <ContentWrapper>
                 <div className="content">
                   <div className="left">
-                    {data.poster_path ? (
+                    {data?.poster_path ? (
                       <Img
                         className="posterImg"
-                        src={url.backdrop + data.poster_path}
+                        src={url.backdrop + data?.poster_path}
                       />
                     ) : (
                       <Img className="posterImg" src={PosterFallback} />
@@ -61,10 +61,10 @@ const DetailsBanner = ({ video, crew }) => {
                   </div>
                   <div className="right">
                     <div className="title">
-                      {`${data.name || data.title}
+                      {`${data?.name || data?.title}
                         (${dayjs(data?.release_date).format("YYYY")})`}
                     </div>
-                    <div className="subtitle">{data.tagline}</div>
+                    <div className="subtitle">{data?.tagline}</div>
 
                     <Genres data={_genres} />
 
@@ -83,29 +83,30 @@ const DetailsBanner = ({ video, crew }) => {
 
                     <div className="overview">
                       <div className="heading">Overview</div>
-                      <div className="description">{data.overview}</div>
+                      <div className="description">{data?.overview}</div>
                     </div>
 
                     <div className="info">
-                      {data.status && (
+                      {data?.status && (
                         <div className="infoItem">
                           <span className="text bold">Status: </span>
-                          <span className="text">{data.status}</span>
+                          <span className="text">{data?.status}</span>
                         </div>
                       )}
-                      {data.release_date && (
+                      {data?.release_date && (
                         <div className="infoItem">
                           <span className="text bold">Release Date: </span>
                           <span className="text">
-                            {dayjs(data.release_date).format("MMMM D, YYYY")}
+                            {dayjs(data?.release_date).format("MMMM D, YYYY")}
                           </span>
                         </div>
                       )}
-                      {data.runtime && (
+                      {data?.runtime && (
                         <div className="infoItem">
-                          <span className="text bold">Runtime: </span>
+                          <span className="text bold">Runtime: {" "}
+                          </span>
                           <span className="text">
-                            {toHoursAndMinutes(data.runtime)}
+                            {toHoursAndMinutes(data?.runtime)}
                           </span>
                         </div>
                       )}
@@ -113,7 +114,8 @@ const DetailsBanner = ({ video, crew }) => {
 
                     {director?.length > 0 && (
                       <div className="info">
-                        <span className="text bold">Director: </span>
+                        <span className="text bold">Director: {" "}
+                        </span>
                         <span className="text">
                           {director?.map((d, i) => (
                             <span key={i}>
@@ -127,7 +129,8 @@ const DetailsBanner = ({ video, crew }) => {
 
                     {writer?.length > 0 && (
                       <div className="info">
-                        <span className="text bold">Writer: </span>
+                        <span className="text bold">Writer: {" "} 
+                        </span>
                         <span className="text">
                           {writer?.map((d, i) => (
                             <span key={i}>
@@ -140,7 +143,8 @@ const DetailsBanner = ({ video, crew }) => {
                     )}
                     {data?.created_by?.length > 0 && (
                       <div className="info">
-                        <span className="text bold">Creator: </span>
+                        <span className="text bold">Creator: {" "} 
+                        </span>
                         <span className="text">
                           {data?.created_by?.map((d, i) => (
                             <span key={i}>
